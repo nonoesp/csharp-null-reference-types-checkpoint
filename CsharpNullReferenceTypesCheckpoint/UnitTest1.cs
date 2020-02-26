2e3e0a097bc02936e07d885bb58e44ff1a81fce0
Nono Martinez Alonso <nono.martinez.alonso@autodesk.com>
2020-02-26 10:31:58 +0100

    First commit

---
+++ CsharpNullReferenceTypesCheckpoint/UnitTest1.cs
@@ -0,0 +1,31 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CsharpNullReferenceTypesCheckpoint
+{
+    [TestClass]
+    public class MyClass
+    {
+        [TestMethod]
+        public void Test1()
+        {
+
+            var student = new Student()
+            {
+                FirstName = "John"
+            };
+
+            string result = ($"The student is called {student.FirstName}.");
+
+            Assert.AreEqual("The student is called John.", result);
+        }
+    }
+}
+
+    public class Student
+{
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? Email { get; set; }
+    public char? Grade { get; set; }
+}
+
